edb1b9f891603bd317e323b427456cd1109b1bb4
RemKeeper <78486275+RemKeeper@users.noreply.github.com>
2024-01-24 17:47:56 +0800

    Update main.go

--- main.go
+++ main.go
@@ -20,17 +20,13 @@ var (
 	CloseConn           chan bool
 )
 
-//const (
-//	AppID        int64 = 123456789
-//	AccessKey          = ""
-//	AccessSecret       = ""
-//)
-
 const (
-	AppID        int64 = 1662382413323
-	AccessKey          = "CzTDnPBEt17lbQjJBjCxwLzz"
-	AccessSecret       = "BEZOssCVUnLD76YnPCtVa8Qz8Tn5r4"
+	AppID        int64 = 123456789
+	AccessKey          = ""
+	AccessSecret       = ""
 )
+
+
 
 func main() {
 	//CleanOldVersion()
